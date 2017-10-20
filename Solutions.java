372fbfcdd54eb31e5afeff36e03011075f78ee58
axis00 <dev.mintgames@gmail.com>
2017-10-20 15:41:44 +0800

    revised newyear sol

--- Solutions.java
+++ Solutions.java
@@ -2,15 +2,17 @@
 public class Solutions{
 
 	public static void main(String[] args) {
+	    Scanner s = new Scanner(System.in);
+	    System.out.print(NewYear(s.nextInt(),s.nextInt()));
 		
 	}
 
-	public int NewYear(int n, int k){
-    
+	public static int NewYear(int n, int k){
+        int total=0;
 	    for(int i = 0; i <= n; i++){
 	        total += i * 5;
 	        if(total+k > 240 - ((i + 1)*5)){
-	            return 0;
+	            return i;
 	        }
 	    }
 	    

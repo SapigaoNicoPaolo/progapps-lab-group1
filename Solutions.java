c2e5353cdad3256b7c9281194552bb7775b4349f
axis00 <dev.mintgames@gmail.com>
2017-10-20 15:33:05 +0800

    added newyear problem solution

--- Solutions.java
+++ Solutions.java
@@ -1,1 +1,19 @@
 //add solutions here
+public class Solutions{
+
+	public static void main(String[] args) {
+		
+	}
+
+	public int NewYear(int n, int k){
+    
+	    for(int i = 0; i <= n; i++){
+	        total += i * 5;
+	        if(total+k > 240 - ((i + 1)*5)){
+	            return 0;
+	        }
+	    }
+	    
+	    return n;
+	}
+}
